6f740942b07c65c5faf1e577d60566d7aa75c85a
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2022-11-21 20:15:32 +0100

    auth form style update

--- src/pages/profile/auth/AuthForm.jsx
+++ src/pages/profile/auth/AuthForm.jsx
@@ -2,18 +2,20 @@ const AuthForm = ({ submitValue }) => {
     return (
         <form className="auth-form">
             <input
+                className="auth-input"
                 type="text"
                 name="login-email"
                 id="email-input"
                 placeholder="Podaj Email"
             />
             <input
+                className="auth-input"
                 type="password"
                 name="login-password"
                 id="password-input"
                 placeholder="Podaj Hasło"
             />
-            <input type="submit" value={submitValue} />
+            <input type="submit" className="auth-submit" value={submitValue} />
         </form>
     );
 };
