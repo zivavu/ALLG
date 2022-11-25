d7236f93fcedc07ce7053b5b5fdc02d01a08f688
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2022-11-25 15:08:10 +0100

    image loading animation

--- src/components/advertsListSection/AdvertElement.jsx
+++ src/components/advertsListSection/AdvertElement.jsx
@@ -1,24 +1,36 @@
 import { getDownloadURL, ref } from 'firebase/storage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { FirebaseStorage } from '../../config/firebase-config';
 
 const AdvertElement = ({ advert }) => {
     const [imageURL, setImageURL] = useState('');
+    const [imageLoading, setImageLoading] = useState(true);
+
     const downloadImage = () => {
+        setImageLoading(true);
         const imagePathRef = ref(FirebaseStorage, `${advert.imagePath}`);
         getDownloadURL(imagePathRef)
             .then((url) => {
                 setImageURL(url);
+                setImageLoading(false);
             })
             .catch((error) => {
                 console.log(error);
             });
     };
-    downloadImage();
+
+    useEffect(() => {
+        downloadImage();
+    }, []);
+
     return (
         <Link className="advert-element-container" to={`/advert/${advert.id}`}>
-            <img src={imageURL} alt="Przedmiot ogłoszenia" />
+            {imageLoading ? (
+                <div class="lds-hourglass"></div>
+            ) : (
+                <img src={imageURL} alt="Przedmiot ogłoszenia" />
+            )}
             <div className="home-advert-info">
                 <div className="home-advert-title">{advert.title}</div>
                 <div className="home-advert-details">
